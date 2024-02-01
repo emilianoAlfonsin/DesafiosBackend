e3c88370fc81d297244d3c462774fc7a9f520bf0
emilianoAlfonsin <emiliano.a.83@gmail.com>
2024-02-01 13:50:57 -0300

    Asincronismo en metodos getProduct y addProduct

--- productManager.js
+++ productManager.js
@@ -1,22 +1,34 @@
+const fs = require('fs')
+
 // Creación de la clase ProductManager
 class ProductManager {
 
-    constructor () { // no se si debería colocar algo como parametro
+    constructor (path) { 
+        this.path = path
         this.products = []
     }
 
+    // Método para obtener todos los productos del array de productos
+    getProducts = async() =>{
+        const data = await fs.promises.readFile(this.path, 'utf-8')
+        const products = data ? JSON.parse(data) : []
+        return products
+    }  
+
     // Método para agregar productos al array de productos
-    addProduct = (title, description, price, tumbnail, code, stock) => {
-        if (!title || !description || !price || !tumbnail || !code || !stock) {
+    addProduct = async (title, description, price, tumbnail, code, stock, offer) => {
+        if (!title || !description || !price || !tumbnail || !code || !stock || !offer) {
             return console.log("Por favor verifique que todos los campos del producto se encuentren completos")
         }
 
-        
+        const products = await this.getProducts()
+
         // Cálculo del id del producto para que no se repita en el array de productos
-        const productId = this.products.length > 0
-        ? this.products[this.products.length - 1].id + 1 // Accediendo al id del ultimo elemento del array
-        : 1;
-        
+        const productId = products.length > 0
+        ? products[products.length - 1].id + 1 // Accediendo al id del ultimo elemento del array
+        : 1
+
+
         const product = {
             id : productId,
             title,
@@ -24,30 +36,27 @@ class ProductManager {
             price,
             tumbnail,
             code,
-            stock
+            stock,
+            offer
         }
         
         // Validación de que el código del producto no se repita en el array de productos
-        const codeControl = this.products.find((prod) => prod.code === code )
+        const codeControl = products.find((prod) => prod.code === code )
         if (codeControl) {
         return console.log("El código del producto ya existe")
         } 
         
         // Agregando el producto al array de productos
-        this.products.push(product)
 
-        return product
+        products.push(product)
+        await fs.promises.writeFile(this.path, JSON.stringify(products))
+
+        return console.log(`${product.title} agregado`)
     }
 
-    // Método para obtener todos los productos del array de productos
-    getProducts(){
-        this.products.length === 0
-        ? console.log("No hay productos cargados")
-        : console.log(this.products)  // Imprimiendo el array de productos en consola
-    }  
 
     // Método para obtener un producto del array de productos por su id
-    getProductByID(id){
+    getProductByID = (id) => {
         const prodById = this.products.find((prod) => prod.id == id)
         prodById 
         ? console.log(prodById) 
@@ -55,22 +64,24 @@ class ProductManager {
     }
 
     // Método para eliminar un producto del array de productos por su id
-    deleteProductById(id){
+    deleteProductById = (id) => {
         const prodById = this.products.find((prod) => prod.id == id)
         prodById
         ? this.products.splice(prodById, 1) && console.log("Producto eliminado")
         : console.log("No se encuentra el producto seleccionado")
     }
+
+}
+// Testeando funcionamiento
+async function run () {
+
+const manager = new ProductManager('./products.json')
+
+// Agregando productos
+
+await manager.addProduct('Producto 10', 'Descripción del producto 10', 100, 'imagen1.jpg', 'ABC10', 10, true)
+
+await manager.getProducts()
 }
 
-// Testeando funcionamiento
-const manager = new ProductManager()
-manager.getProducts()
-manager.addProduct("producto prueba1", "Este es un producto prueba", 200, "Sin imágen", "abc1", 25)
-manager.addProduct("producto prueba2", "Este es un producto prueba", 300, "Sin imágen", "abc2", 25)
-manager.addProduct("producto prueba3", "Este es un producto prueba", 400, "Sin imágen", "abc3", 25)
-manager.getProducts()
-manager.addProduct("producto prueba3", "Este es un producto prueba", 400, "Sin imágen", "abc3", 25)
-manager.deleteProductById(1)
-manager.getProducts()
-manager.getProductByID(2)
+run()
