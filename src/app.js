60eecaddf24133cc6db6b3067631bd7bad2fedd0
emilianoAlfonsin <emiliano.a.83@gmail.com>
2024-02-18 12:41:02 -0300

    Servidor, rutas y métodos para el manejo de productos

--- src/app.js
+++ src/app.js
@@ -6,10 +6,17 @@ const app = express()
 const port = 8080
 const productManager = new ProductManager('./products.json')
 
+app.listen(port, () => {
+    console.log(`Server corriendo en el puerto ${port}`)
+})
+
+app.use(express.json())
+
 app.use(express.urlencoded({encoded:true}))
 
 
-app.get('/products', async (req, res) => {
+
+app.get('api/products', async (req, res) => {
     const limit = req.query.limit
     const products = await productManager.getProducts()
     limit > 0
@@ -18,11 +25,24 @@ app.get('/products', async (req, res) => {
 })
 
 
-app.get('/products/:pid', async (req, res) => {
+app.get('api/products/:pid', async (req, res) => {
     const product = await productManager.getProductById(req.params.pid)
     res.json(product)
 })
 
-app.listen(port, () => {
-    console.log(`Server corriendo en el puerto ${port}`)
+app.post('api/products', async (req, res) => {
+    const product = req.body
+    await productManager.addProduct(product.title, product.description, product.price, product.thumbnail, product.code, product.stock, product.offer)
+    res.json(product)
+})
+
+app.put('api/products/:pid', async (req, res) => {
+    const product = req.body
+    await productManager.updateProduct(req.params.pid, product.title, product.description, product.price, product.thumbnail, product.code, product.stock, product.offer)
+    res.json(product)
+})
+
+app.delete('api/products/:pid', async (req, res) => {
+    await productManager.deleteProductById(req.params.pid)
+    res.json("Producto eliminado")
 })
